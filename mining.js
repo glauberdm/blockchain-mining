4e7048c1b8d0fe90e489db8cf72baefcdcbdb237
Glauber Monteiro <glauberbcc@gmail.com>
2019-08-22 15:03:57 -0300

    up

---
+++ mining.js
@@ -0,0 +1,19 @@
+const CryptoJS = require('crypto-js');
+
+var nonce = 0;
+var start = '000';
+var hash = '';
+
+// enquanto o hash não começa com 0's suficientes
+while (hash.substring(0, start.length) != start) {
+  // incrementa nonce
+  nonce++;
+  // calcula hash
+  hash = CryptoJS.SHA256('blockchain' + nonce).toString();
+  // imprime nonce apenas para verificar que está rodando
+  if (nonce % 100000 == 0) console.log('nonce at: ' + nonce);
+}
+
+console.log('=========================');
+console.log('found hash: ' + hash);
+console.log('nonce: ' + nonce);
